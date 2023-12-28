96aed8e14e1c0f46d52addc7a63ef44388444957
tetn39 <tetn39@gmail>
2023-12-28 17:49:23 +0900

    検索のAPIを取得して加工できるようになった(playlistに反映)

--- projects/melotus/views.py
+++ projects/melotus/views.py
@@ -30,7 +30,7 @@ def index(request):
 
 def songs(request):
     context = {}
-
+    
     if request.user.is_authenticated:
         print('ログイン済み')
         requested_user_id = request.user.id
@@ -52,6 +52,8 @@ def songs(request):
         print('ログインしていない')
 
     song_name = request.POST.get('song_name')
+    if song_name is None:
+        song_name = ''
     context['song_name'] = song_name
     
     return render(request, 'songs.html', context)
@@ -121,18 +123,23 @@ def playlist(request):
 
     # ここで検索のを試す
     # END_POINT = 'https://api.spotify.com/v1/me/albums?limit=3' 
-    END_POINT = 'https://api.spotify.com/v1/search?q=BTS&type=album&market=JP&limit=3'  # 中のやつが違うため、どうにかする
+    END_POINT = 'https://api.spotify.com/v1/search?q=BTS&type=album&market=JP&limit=3'
     # https://developer.spotify.com/documentation/web-api/reference/search 参考サイト
 
     res = requests.get(END_POINT, headers=header_params)
     data = res.json()
     context = {
-        'all_data': data['items'][0]['album'],
-        'album_name': data['items'][0]['album']['name'],
-        'album_img': data['items'][0]['album']['images'][0]['url'],
-        'album_url': data['items'][0]['album']['external_urls']['spotify'],
-        'artist_name': data['items'][0]['album']['artists'][0]['name'],
-        'artist_url': data['items'][0]['album']['artists'][0]['external_urls']['spotify'],
-        
+        'songs': [],
     }
+
+    for i in range(3):
+        context['songs'].append({
+            'album_name': data['albums']['items'][i]['name'],
+            'album_img': data['albums']['items'][i]['images'][0]['url'],
+            'album_url': data['albums']['items'][i]['external_urls']['spotify'],
+            'artist_name': data['albums']['items'][i]['artists'][0]['name'],
+            'artist_url': data['albums']['items'][i]['artists'][0]['external_urls']['spotify'],
+        })
+    
+    
     return render(request, 'old/playlist.html', context)

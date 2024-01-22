e133b27fa90787274ffe70df34eeec2b0ce32e1e
pgn111 <yohto089@gmail.com>
2024-01-22 13:26:25 +0900

    search.jsにsend.jsの機能を追加

--- projects/static/js/search.js
+++ projects/static/js/search.js
@@ -176,5 +176,40 @@ async function initializeSearch() {
   }
 }
 
+function send() {
+  console.log("実行");
+  var csrftoken = getCookie("csrftoken");
+  fetch("/js_py/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRFToken": csrftoken,
+    },
+    body: JSON.stringify({ selectedUris: selectedUris }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Success:", data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+function getCookie(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie !== "") {
+    var cookies = document.cookie.split(";");
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = cookies[i].trim();
+      if (cookie.substring(0, name.length + 1) === name + "=") {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
+
 // 初期化処理を実行
 initializeSearch();

ee34b6d34f7ce2f317cb06de46f4e7248c2aed1c
tetn39 <tetn39@gmail.com>
2024-02-01 10:16:37 +0900

    db更新

--- projects/melotus/admin.py
+++ projects/melotus/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import music_status, history, melotus_data
+from .models import music_status, history, melotus_data, spotify_data
 
 admin.site.register(music_status)
 admin.site.register(history)
 admin.site.register(melotus_data)
+admin.site.register(spotify_data)
--- projects/melotus/models.py
+++ projects/melotus/models.py
@@ -30,7 +30,15 @@ class history(models.Model):
     tempo = models.FloatField(default=0)
     valence = models.FloatField(default=0)
 
+class spotify_data(models.Model):
+    spotify_id = models.AutoField(primary_key=True)
+    user_name = models.CharField(max_length=50)
+    image_url = models.CharField(max_length=100)
+    country = models.CharField(max_length=2)
+
 class melotus_data(models.Model):
     melotus_id = models.AutoField(primary_key=True)
+    spotify_id = models.ForeignKey(spotify_data, on_delete=models.CASCADE, null=True)
     history_id = models.ForeignKey(history, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
+

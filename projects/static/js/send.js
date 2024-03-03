570d0d3bf471f3aec82fc9bb6d1ace878a6c8767
tetn39 <tetn39@gmail>
2024-03-03 17:10:19 +0900

    playlist修正

--- projects/static/js/send.js
+++ projects/static/js/send.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   const items = document.querySelectorAll('.item-list');
-  let visibleCount = 4; // 初期表示項目数
+  let visibleCount = 5; // 初期表示項目数
 
   // 最初の表示項目数以降のアイテムに.hiddenクラスを追加して非表示にする
   for (let i = visibleCount; i < items.length; i++) {
@@ -10,23 +10,35 @@ document.addEventListener('DOMContentLoaded', function() {
   const openBtn = document.querySelector('.open-btn');
   if (openBtn) {
     openBtn.addEventListener('click', function() {
-      const nextEndIndex = visibleCount + 4;
-      for (let i = visibleCount; i < nextEndIndex && i < items.length; i++) {
-        items[i].classList.remove('hidden'); // .hiddenクラスを削除して表示
-      }
-      visibleCount += 4; // 表示済みのカウントを更新
+      // 「もっと見る」ボタンがクリックされた時の処理
+      if (openBtn.textContent === 'もっと見る') {
+        const nextEndIndex = visibleCount + 5;
+        for (let i = visibleCount; i < nextEndIndex && i < items.length; i++) {
+          items[i].classList.remove('hidden'); // .hiddenクラスを削除して表示
+        }
+        visibleCount += 5; // 表示済みのカウントを更新
 
-      if (visibleCount >= items.length) {
-        openBtn.classList.add('hidden'); // すべて表示したらボタンを非表示に
+        // すべて表示したらボタンのテキストを変更
+        if (visibleCount >= items.length) {
+          openBtn.textContent = '表示を減らす';
+        }
+      } else {
+        // 「表示を減らす」がクリックされた時の処理
+        items.forEach((item, index) => {
+          if (index >= 5) item.classList.add('hidden');
+        });
+        visibleCount = 5; // 表示カウントをリセット
+        openBtn.textContent = 'もっと見る'; // ボタンのテキストをリセット
       }
     });
   }
 
-  // アイテムの総数が4以下の場合はボタンを非表示に（.hiddenクラスを追加）
-  if (items.length <= 4) {
+  // アイテムの総数が5以下の場合はボタンを非表示に（.hiddenクラスを追加）
+  if (items.length <= 5) {
     openBtn.classList.add('hidden');
   }
 });
+
 
 
 function send(selectedId) {

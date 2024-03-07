e04ebf3217fbc0b6cd4d37dd94f9811d3b56be67
dev-yohto <yohto089@gmail.com>
2024-03-07 21:16:19 +0900

    searchjsリファクタリング、エラー出たら戻します

--- projects/static/js/search.js
+++ projects/static/js/search.js
@@ -1,11 +1,13 @@
-let selectedTracks = [];
-let selectedUris = [];
+// アプリケーションの状態を管理するオブジェクト
+const state = {
+  selectedTracks: [],
+  selectedUris: [],
+  searchTimeout: null,
+};
 
+// Spotifyのアクセストークンを取得する非同期関数
 async function getAccessToken() {
-  // CSRFトークンを取得
   const csrftoken = getCookie("csrftoken");
-
-  // get_token/ からGETしてアクセストークンを取得する
   const response = await fetch("/get_token/", {
     method: "GET",
     headers: {
@@ -13,12 +15,11 @@ async function getAccessToken() {
       "X-CSRFToken": csrftoken,
     },
   });
-
   const data = await response.json();
   return data;
 }
 
-// Spotifyでトラックを検索する関数
+// Spotifyでトラックを検索する非同期関数
 async function searchSpotify(access_token, query) {
   const response = await fetch(
     `https://api.spotify.com/v1/search?q=${query}&type=track&limit=5`,
@@ -30,16 +31,15 @@ async function searchSpotify(access_token, query) {
   return await response.json();
 }
 
-// トラックを選択リストに追加する関数
+// 選択されたトラックをリストに追加する関数
 function addTrackToList(track) {
-  // 既に選択されているかを確認
-  const isAlreadySelected = selectedUris.includes(track.uri.substr(14));
+  const isAlreadySelected = state.selectedUris.includes(track.uri.substr(14));
 
   if (!isAlreadySelected) {
-    selectedTracks.push(track);
-    selectedUris.push(track.uri.substr(14));
+    state.selectedTracks.push(track);
+    state.selectedUris.push(track.uri.substr(14));
     renderSelectedTracks();
-    console.log("Selected URIs:", selectedUris);
+    console.log("Selected URIs:", state.selectedUris);
   } else {
     console.log("既に選択されています");
   }
@@ -49,52 +49,37 @@ function addTrackToList(track) {
 function renderSelectedTracks() {
   renderSelectedTracksWithIndex();
 
-  // 選択された曲がある場合
-  if (selectedTracks.length > 0) {
-    // 「選択済みの曲」ヘッダーを表示
-    document.getElementById("selectedTracksHeader").style.display = "block";
+  const selectedTracksHeader = getElementById("selectedTracksHeader");
+  const diagnosisButtonWrap = getElementById("diagnosisButtonWrap");
 
-    // 「診断する」ボタンのラッパーを表示
-    document.getElementById("diagnosisButtonWrap").style.display = "block";
-  } else {
-    // 選択された曲がない場合
-    // 「選択済みの曲」ヘッダーを非表示
-    document.getElementById("selectedTracksHeader").style.display = "none";
-
-    // 「診断する」ボタンのラッパーを非表示
-    document.getElementById("diagnosisButtonWrap").style.display = "none";
-  }
+  toggleElementDisplay(selectedTracksHeader, state.selectedTracks.length > 0);
+  toggleElementDisplay(diagnosisButtonWrap, state.selectedTracks.length > 0);
 }
 
 // 選択されたトラックを配列番号と共に表示する関数
 function renderSelectedTracksWithIndex() {
-  const listContainer = document.getElementById("selectedTracks");
+  const listContainer = getElementById("selectedTracks");
   listContainer.innerHTML = "";
 
-  selectedTracks.forEach((track, index) => {
+  state.selectedTracks.forEach((track, index) => {
     const trackDiv = createTrackDiv(track, index);
     const indexLabel = createIndexLabel(index);
     trackDiv.insertBefore(indexLabel, trackDiv.firstChild);
     const deleteButton = createDeleteButton(index);
 
-    // deleteButtonのクリックイベントをtrackDivに適用
     trackDiv.addEventListener("click", () => {
       removeTrackFromList(index);
     });
 
     trackDiv.appendChild(deleteButton);
-    if (listContainer.innerHTML === "") {
-      listContainer.appendChild(trackDiv);
-    } else {
-      listContainer.insertBefore(trackDiv, listContainer.firstChild);
-    }
+    listContainer.insertBefore(trackDiv, listContainer.firstChild);
   });
 }
 
 // 配列番号を表示するためのラベルを作成
 function createIndexLabel(index) {
   const indexLabel = document.createElement("div");
-  indexLabel.textContent = `#${index + 1}`; // 配列番号は1から始まるようにするため +1
+  indexLabel.textContent = `#${index + 1}`;
   indexLabel.classList.add("index-label");
   return indexLabel;
 }
@@ -102,9 +87,6 @@ function createIndexLabel(index) {
 // 選択されたトラックを表示するためのDIVを作成する関数
 function createTrackDiv(track, index = 0) {
   const trackDiv = document.createElement("div");
-
-  // indexが偶数の場合はevenクラスを、奇数の場合はoddクラスを追加
-  console.log("Index value:", index);
 
   if (index % 2 === 0) {
     trackDiv.classList.add("even");
@@ -122,7 +104,6 @@ function createTrackDiv(track, index = 0) {
   const trackInfo = document.createElement("div");
   trackInfo.classList.add("track-info");
 
-  // トラック情報のための三つの div を作成
   const trackNameDiv = document.createElement("div");
   trackNameDiv.textContent = track.name;
   trackNameDiv.classList.add("track-name");
@@ -135,7 +116,6 @@ function createTrackDiv(track, index = 0) {
   artistNameDiv.textContent = track.artists[0].name;
   artistNameDiv.classList.add("artist-name");
 
-  // 三つの div を trackInfo に追加
   trackInfo.appendChild(trackNameDiv);
   trackInfo.appendChild(albumNameDiv);
   trackInfo.appendChild(artistNameDiv);
@@ -147,7 +127,6 @@ function createTrackDiv(track, index = 0) {
 
 // 選択されたトラックを削除するボタンを作成する関数
 function createDeleteButton(index) {
-  // 外側にdivを追加
   const deleteButtonWrap = document.createElement("div");
   deleteButtonWrap.classList.add("delete-button-wrap");
 
@@ -168,108 +147,119 @@ function createDeleteButton(index) {
 
 // 選択されたトラックをリストから削除する関数
 function removeTrackFromList(index) {
-  selectedTracks.splice(index, 1);
-  selectedUris.splice(index, 1);
+  state.selectedTracks.splice(index, 1);
+  state.selectedUris.splice(index, 1);
   renderSelectedTracks();
-  console.log("Selected URIs:", selectedUris);
+  console.log("Selected URIs:", state.selectedUris);
 }
 
-let searchTimeout;
+// HTML要素を取得する関数
+function getElementById(elementId) {
+  return document.getElementById(elementId);
+}
 
-// 初期化処理を行う関数
+// HTML要素の表示を切り替える関数
+function toggleElementDisplay(element, shouldDisplay) {
+  element.style.display = shouldDisplay ? "block" : "none";
+}
+
+// 検索機能の初期化
 async function initializeSearch() {
   const accessToken = await getAccessToken();
-  const searchInput = document.getElementById("searchInput");
-  const searchResults = document.getElementById("searchResults");
-  const selectedTracksHeader = document.getElementById("selectedTracksHeader");
-  const diagnosisButtonWrap = document.getElementById("diagnosisButtonWrap");
+  const searchInput = getElementById("searchInput");
 
-  // 初期状態で非表示にする
-  selectedTracksHeader.style.display = "none";
-  diagnosisButtonWrap.style.display = "none";
+  const selectedTracksHeader = getElementById("selectedTracksHeader");
+  const diagnosisButtonWrap = getElementById("diagnosisButtonWrap");
+
+  toggleElementDisplay(selectedTracksHeader, false);
+  toggleElementDisplay(diagnosisButtonWrap, false);
 
   searchInput.addEventListener("input", async (event) => {
-    clearTimeout(searchTimeout); // 前回のタイマーをクリア
+    clearTimeout(state.searchTimeout);
 
     const query = event.target.value;
     if (query.length > 0) {
-      // 一定時間入力がない場合にAPIを呼び出す
-      searchTimeout = setTimeout(async () => {
+      state.searchTimeout = setTimeout(async () => {
         const result = await searchSpotify(accessToken.access_token, query);
         displayResults(result.tracks.items);
-      }, 500); //500ミリ秒待機してからAPI呼び出し
+      }, 500);
     } else {
-      searchResults.innerHTML = "";
+      getElementById("searchResults").innerHTML = "";
     }
   });
-
-  // 検索結果を表示する関数
-  function displayResults(tracks) {
-    searchResults.innerHTML = "";
-    tracks.forEach((track, index) => {
-      const trackDiv = createTrackDiv(track, index);
-      const addButton = createAddButton(track);
-
-      // addButtonのクリックイベントをtrackDivに適用
-      trackDiv.addEventListener("click", () => {
-        addTrackToList(track);
-      });
-
-      trackDiv.appendChild(addButton);
-      searchResults.appendChild(trackDiv);
-    });
-
-    // 選択された曲がある場合、初期状態で表示にする
-    renderSelectedTracks();
-    createSearchTitle();
-  }
-
-  // 検索結果のタイトルを生成する関数
-  function createSearchTitle() {
-    const searchTitleDiv = document.createElement("div");
-    searchTitleDiv.classList.add("search_title");
-
-    const titleText = ["タイトル", "アルバム", "アーティスト"];
-    titleText.forEach((text) => {
-      const paragraph = document.createElement("p");
-      paragraph.textContent = text;
-      searchTitleDiv.appendChild(paragraph);
-    });
-
-    searchResults.insertBefore(searchTitleDiv, searchResults.firstChild);
-  }
-
-  // トラックを選択するボタンを作成する関数
-  function createAddButton(track) {
-    // 外側にdivを追加
-    const addButtonWrap = document.createElement("div");
-    addButtonWrap.classList.add("add-button-wrap");
-
-    const addButton = document.createElement("span");
-    addButton.classList.add("add-button");
-
-    const addButtonIcon = document.createElement("span");
-
-    addButton.appendChild(addButtonIcon);
-    addButtonWrap.appendChild(addButton);
-
-    return addButtonWrap;
-  }
 }
 
+// 検索結果を表示する関数
+function displayResults(tracks) {
+  const searchResults = getElementById("searchResults");
+  searchResults.innerHTML = "";
+
+  tracks.forEach((track, index) => {
+    const trackDiv = createTrackDiv(track, index);
+    const addButton = createAddButton(track);
+
+    trackDiv.addEventListener("click", () => {
+      addTrackToList(track);
+    });
+
+    trackDiv.appendChild(addButton);
+    searchResults.appendChild(trackDiv);
+  });
+
+  renderSelectedTracks();
+  createSearchTitle();
+}
+
+// 検索結果のタイトルを生成する関数
+function createSearchTitle() {
+  const searchTitleDiv = document.createElement("div");
+  searchTitleDiv.classList.add("search_title");
+
+  const titleText = ["タイトル", "アルバム", "アーティスト"];
+  titleText.forEach((text) => {
+    const paragraph = document.createElement("p");
+    paragraph.textContent = text;
+    searchTitleDiv.appendChild(paragraph);
+  });
+
+  getElementById("searchResults").insertBefore(
+    searchTitleDiv,
+    searchResults.firstChild
+  );
+}
+
+// トラックを選択するボタンを作成する関数
+function createAddButton(track) {
+  const addButtonWrap = document.createElement("div");
+  addButtonWrap.classList.add("add-button-wrap");
+
+  const addButton = document.createElement("span");
+  addButton.classList.add("add-button");
+
+  const addButtonIcon = document.createElement("span");
+
+  addButton.appendChild(addButtonIcon);
+  addButtonWrap.appendChild(addButton);
+
+  return addButtonWrap;
+}
+
+// 選択されたトラックをサーバーに送信する関数
 function send() {
   console.log("実行");
-  var csrftoken = getCookie("csrftoken");
+  const csrftoken = getCookie("csrftoken");
+
   fetch("/js_py/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       "X-CSRFToken": csrftoken,
     },
-    body: JSON.stringify({ selectedUris: selectedUris }),
+    body: JSON.stringify({ selectedUris: state.selectedUris }),
   })
     .then((response) => response.json())
     .then((data) => {
+      // サーバーからのデータをログに出力
       console.log("jsから:", data.uris);
       console.log("pyから:", data.user_status);
       console.log("pyから:", data.recommended_music);
@@ -283,12 +273,13 @@ function send() {
     });
 }
 
+// Cookieから値を取得する関数
 function getCookie(name) {
-  var cookieValue = null;
+  let cookieValue = null;
   if (document.cookie && document.cookie !== "") {
-    var cookies = document.cookie.split(";");
-    for (var i = 0; i < cookies.length; i++) {
-      var cookie = cookies[i].trim();
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
       if (cookie.substring(0, name.length + 1) === name + "=") {
         cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
         break;
@@ -298,5 +289,5 @@ function getCookie(name) {
   return cookieValue;
 }
 
-// 初期化処理を実行
+// アプリケーションの初期化処理を実行
 initializeSearch();
